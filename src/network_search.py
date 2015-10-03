fe4b4d33b9fab559e784ca7c551879ea4e246e3c
Fernando Carrillo <fernando@carrillo.at>
2015-10-03 16:17:17 -0400

    Started notbook for twitter info gathering example.

--- src/network_search.py
+++ src/network_search.py
@@ -9,7 +9,6 @@ from users import Twitter_user
 from twitter_auth import Twitter_auth
 from json_parser import ParseJSON
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import exists
